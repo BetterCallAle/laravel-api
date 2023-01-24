940fe5e8ae6434e9d3222706400c47a669d5f804
BetterCallAle <dap22091998@gmail.com>
2023-01-24 16:35:59 +0100

    Add: Mail

---
+++ resources/views/email/new-contact-mail.blade.php
@@ -0,0 +1,7 @@
+<h1>Ciao Amministratore!</h1>
+<div>
+    Hai appena ricevuto un messaggio da: <br>
+    Nome: {{ $lead->name }} <br>
+    Email: {{ $lead->email }} <br>
+    Messaggio: {{ $lead->message }}
+</div>
